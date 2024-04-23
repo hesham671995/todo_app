809f1765acb56561c20b9cb3d8aa1e6cefeed932
Hesham Kamal <hesham.kamal@eden-tech.io>
2024-04-23 17:23:08 +0200

    add-decorator-@ApiProperty()-to-create-users-dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,16 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateUserDto {
     
+    @ApiProperty()
     @IsString()
     firstName : string;
 
+    @ApiProperty()
     @IsString()
     lastName : string;
 
+    @ApiProperty()
     @IsEmail()
     email : string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     password : string;
